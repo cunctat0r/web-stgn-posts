6ad5974ff93e979769686ac5be56fcfa5291099e
Slava Petrov <slavik--petrov@yandex.ru>
2018-06-05 08:27:21 +0000

    Add form to create post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,35 @@
+# Posts controller
 class PostsController < ApplicationController
-  def index
-  end
+  before_action :set_post, only: [:show, :update, :destroy]
+
+  def index; end
 
   def new
+
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
-  def show
+  def show; end
+
+  def update; end
+
+  def destroy; end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:num_tower, :line_name)
   end
 
-  def update
-  end
-
-  def destroy
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
