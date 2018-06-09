51959b29f2fa147bc873882779cfa7dd1d5bfc89
Slava Petrov <slavik--petrov@yandex.ru>
2018-06-09 08:21:38 +0000

    Add possibility to log in and out

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 # Posts controller
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
